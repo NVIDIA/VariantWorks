6450190fa53b31627a26be5e4590cee2a921f20f
Joyjit Daw <jdaw@nvidia.com>
2020-04-09 21:53:58 -0400

    [dataset] add pyVCF based label loading
    1. load training labels using VCF file
    2. fix bug with number of training classes
    3. updated optimizer to adam

--- claragenomics/variantworks/dataset.py
+++ claragenomics/variantworks/dataset.py
@@ -1,10 +1,13 @@
 # Abstract class for creating a dataset from BAM and VCF files
 
 from torch.utils.data import Dataset, DataLoader
+import vcf
 
 from nemo.backends.pytorch.nm import DataLayerNM
 from nemo.utils.decorators import add_port_docs
 from nemo.core.neural_types import ChannelType, LabelsType, LossType, NeuralType
+
+from claragenomics.variantworks.base_encoder import base_enum_encoder
 
 class SnpPileupDataType(DataLayerNM):
     """
@@ -38,26 +41,42 @@ class SnpPileupDataType(DataLayerNM):
                 self.bam = bam
                 self.pileup_generator = pileup_generator
 
-                self.labels = []
+                self.labels = self.parse_vcf_labels(labels)
                 #TODO: Load labels and training data
 
             def __len__(self):
                 # TODO: Get length from loaded dataset
-                return 2
+                return len(self.labels)
 
             def __getitem__(self, idx):
                 # TODO: Get chrom, pos, ref, alt, var_type labels from dataset
-                chrom = "1"
-                pos = 240000
-                var_type = 2
-                var_allele = 1
+                chrom, pos, ref, var_type, var_allele, var_all_seq = self.labels[idx]
                 pileup = self.pileup_generator(self.bam, chrom, pos)
+                #print(chrom, pos, ref, var_type, var_allele, var_all_seq)
                 return pileup, var_type, var_allele
+
+            def parse_vcf_labels(self, vcf_file):
+                labels = []
+                assert(vcf_file[-3:] == ".gz") # Check for compressed file
+                vcf_reader = vcf.Reader(open(vcf_file, "rb"))
+                for record in vcf_reader:
+                    assert(record.is_snp) # Right now only supporting SNPs
+                    chrom = record.CHROM
+                    pos = record.POS
+                    ref = record.REF
+                    var_allele = base_enum_encoder[record.ALT[0].sequence]
+                    var_type = 0 # None
+                    if record.num_het > 0:
+                        var_type = 2 # Heterozygous
+                    elif record.num_hom_alt > 0:
+                        var_type = 1 # Homozygous
+
+                    labels.append((chrom, pos, ref, var_type, var_allele, record.ALT[0].sequence))
+                return labels
 
         self.dataloader = DataLoader(DatasetWrapper(bam, labels, pileup_generator),
                                      batch_size = batch_size, shuffle = shuffle,
                                      num_workers = num_workers)
-
 
     def __len__(self):
         return len(self.dataloader)
--- tests/test_simple_vc.py
+++ tests/test_simple_vc.py
@@ -4,6 +4,7 @@ import os
 import nemo
 from nemo import logging
 from nemo.backends.pytorch.common.losses import CrossEntropyLossNM
+from nemo.backends.pytorch.torchvision.helpers import compute_accuracy
 
 from claragenomics.variantworks.dataset import SnpPileupDataType
 from claragenomics.variantworks.pileup_generator import SnpPileupGenerator
@@ -16,28 +17,38 @@ def test_simple_vc():
     nf = nemo.core.NeuralModuleFactory(placement=nemo.core.neural_factory.DeviceType.GPU)
 
     # Generate dataset
-    pileup_generator = SnpPileupGenerator(window_size = 200, max_reads = 1000, channels={"reads"})
+    pileup_generator = SnpPileupGenerator(window_size = 100, max_reads = 100, channels={"reads"})
 
     bam = os.path.join(get_data_folder(), "small_bam.bam")
-    dataset = SnpPileupDataType(bam, None, pileup_generator, batch_size = 2, shuffle = True)
+    labels = os.path.join(get_data_folder(), "candidates.vcf.gz")
+    train_dataset = SnpPileupDataType(bam, labels, pileup_generator, batch_size = 32, shuffle = True)
 
     # Setup loss
     vt_ce_loss = CrossEntropyLossNM(logits_dim=2)
     va_ce_loss = CrossEntropyLossNM(logits_dim=2)
 
     # Neural Network
-    alexnet = AlexNet(num_input_channels=1, num_vt=3, num_alleles=4)
+    alexnet = AlexNet(num_input_channels=1, num_vt=3, num_alleles=5)
 
-    # Create DAG
-    pileups, vt_labels, va_labels = dataset()
+    # Create train DAG
+    pileups, vt_labels, va_labels = train_dataset()
     vt, va = alexnet(pileup=pileups)
     vt_loss = vt_ce_loss(logits=vt, labels=vt_labels)
     va_loss = va_ce_loss(logits=va, labels=va_labels)
 
+
     # SimpleLossLoggerCallback will print loss values to console.
+    def my_print_fn(x):
+        va_output = x[2]
+        va_labels = x[3]
+        acc = compute_accuracy((x[1], va_output, va_labels))
+        logging.info(f'Train VT Loss: {str(x[0].item())}, Train VA Loss: {str(x[1].item())}, Accuracy : {str(acc)}')
+
     callback = nemo.core.SimpleLossLoggerCallback(
-            tensors=[vt_loss, va_loss], print_func=lambda x: logging.info(f'Train Loss: {str(x[0].item())}')
+            tensors=[vt_loss, va_loss, va, va_labels],
+            print_func=my_print_fn,
+            step_freq=1,
             )
 
     # Invoke the "train" action.
-    nf.train([vt_loss, va_loss], callbacks=[callback], optimization_params={"num_epochs": 1, "lr": 0.0003}, optimizer="sgd")
+    nf.train([vt_loss, va_loss], callbacks=[callback], optimization_params={"num_epochs": 10, "lr": 0.001}, optimizer="adam")
