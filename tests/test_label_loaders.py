4886fd94f2aaf9bd3495d407538b857fe9e23d89
Ohad Mosafi <omosafi@nvidia.com>
2020-05-25 10:45:15 +0300

    [tests][label_loader] Mock vcf.Reader.__init__  instead of builtins.open
    Fixes #3

--- tests/test_label_loaders.py
+++ tests/test_label_loaders.py
@@ -1,23 +1,33 @@
-import os
-import builtins
+import vcf
 
 from claragenomics.variantworks.label_loader import VCFLabelLoader
 from claragenomics.variantworks.types import VariantZygosity
 from data.vcf_file_mock import mock_file_input
 
 
-def get_vcf_label_loader_using_mocked_input(mp, vcf_bam_list):
-    with mp.context() as m:
-        m.setattr(builtins, "open", mock_file_input)  # mock file open return value
-        vcf_loader = VCFLabelLoader(vcf_bam_list)
-    return vcf_loader
+class MockPyVCFReader:
+    original_pyvcf_reader_init_function = vcf.Reader.__init__
+
+    @staticmethod
+    def new_vcf_reader_init(self, *args, **kargs):
+        if 'filename' not in kargs:
+            raise RuntimeError('Please use `filename` to initiate vcf.Reader')
+        MockPyVCFReader.original_pyvcf_reader_init_function(self, mock_file_input())
+
+    @staticmethod
+    def get_vcf_label_loader_using_mocked_input(mp, vcf_bam_list):
+        with mp.context() as m:
+            # Mock vcf.Reader.__init__() return value
+            m.setattr(vcf.Reader, "__init__", MockPyVCFReader.new_vcf_reader_init)
+            vcf_loader = VCFLabelLoader(vcf_bam_list)
+        return vcf_loader
 
 
 def test_vcf_loader_snps(monkeypatch):
     """Get all variants from mocked file stream, filter SNPs, multi allele & multi samples
     """
     vcf_bam_tuple = VCFLabelLoader.VcfBamPaths(vcf="/dummy/path.gz", bam="temp.bam", is_fp=False)
-    vcf_loader = get_vcf_label_loader_using_mocked_input(monkeypatch, [vcf_bam_tuple])
+    vcf_loader = MockPyVCFReader.get_vcf_label_loader_using_mocked_input(monkeypatch, [vcf_bam_tuple])
     assert(len(vcf_loader) == 13)
 
 
@@ -25,7 +35,7 @@ def test_vcf_fetch_variant(monkeypatch):
     """Get first variant from mocked VCF file stream.
     """
     vcf_bam_tuple = VCFLabelLoader.VcfBamPaths(vcf="/dummy/path.gz", bam="temp.bam", is_fp=False)
-    vcf_loader = get_vcf_label_loader_using_mocked_input(monkeypatch, [vcf_bam_tuple])
+    vcf_loader = MockPyVCFReader.get_vcf_label_loader_using_mocked_input(monkeypatch, [vcf_bam_tuple])
     try:
         entry = vcf_loader[0]
     except IndexError:
@@ -36,7 +46,7 @@ def test_vcf_load_fp(monkeypatch):
     """Get first variant from false positive mocked VCF file stream and check zygosity.
     """
     vcf_bam_tuple = VCFLabelLoader.VcfBamPaths(vcf="/dummy/path.gz", bam="temp.bam", is_fp=True)
-    vcf_loader = get_vcf_label_loader_using_mocked_input(monkeypatch, [vcf_bam_tuple])
+    vcf_loader = MockPyVCFReader.get_vcf_label_loader_using_mocked_input(monkeypatch, [vcf_bam_tuple])
     for v in vcf_loader:
         assert(v.zygosity == VariantZygosity.NO_VARIANT)
 
@@ -46,6 +56,7 @@ def test_vcf_load_variant_from_multiple_files(monkeypatch):
     """
     first_vcf_bam_tuple = VCFLabelLoader.VcfBamPaths(vcf="/dummy/path.gz", bam="temp.bam", is_fp=False)
     second_vcf_bam_tuple = VCFLabelLoader.VcfBamPaths(vcf="/dummy/path.gz", bam="temp.bam", is_fp=False)
-    vcf_loader = get_vcf_label_loader_using_mocked_input(monkeypatch, [first_vcf_bam_tuple])
-    vcf_loader_2x = get_vcf_label_loader_using_mocked_input(monkeypatch, [first_vcf_bam_tuple, second_vcf_bam_tuple])
+    vcf_loader = MockPyVCFReader.get_vcf_label_loader_using_mocked_input(monkeypatch, [first_vcf_bam_tuple])
+    vcf_loader_2x = MockPyVCFReader.get_vcf_label_loader_using_mocked_input(
+        monkeypatch, [first_vcf_bam_tuple, second_vcf_bam_tuple])
     assert (2 * len(vcf_loader) == len(vcf_loader_2x))
