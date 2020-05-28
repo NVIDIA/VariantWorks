9d64dae86ef4a9d33356f3d81a63fda33e3a9934
Joyjit Daw <jdaw@nvidia.com>
2020-05-28 17:43:49 -0400

    [sample] updated sample to include evaluation

--- samples/simple_snp_caller.py
+++ samples/simple_snp_caller.py
@@ -1,3 +1,19 @@
+#
+# Copyright 2020 NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import os
 import shutil
 import tempfile
@@ -6,7 +22,7 @@ import time
 import nemo
 from nemo import logging
 from nemo.backends.pytorch.common.losses import CrossEntropyLossNM
-from nemo.backends.pytorch.torchvision.helpers import compute_accuracy
+from nemo.backends.pytorch.torchvision.helpers import compute_accuracy, eval_epochs_done_callback, eval_iter_callback
 import torch
 
 from claragenomics.variantworks.dataset import VariantDataLoader
@@ -31,35 +47,42 @@ nf = nemo.core.NeuralModuleFactory(placement=nemo.core.neural_factory.DeviceType
 encoding_layers = [PileupEncoder.Layer.READ]#, PileupEncoder.Layer.BASE_QUALITY, PileupEncoder.Layer.MAPPING_QUALITY]
 pileup_encoder = PileupEncoder(window_size=100, max_reads=100, layers=encoding_layers)
 bam = "/ssd/VariantWorks/end_to_end_workflow_sample_files/HG001.hs37d5.30x.bam"
+zyg_encoder = ZygosityLabelEncoder()
+
+# Setup loss
+
+# Neural Network
+alexnet = AlexNet(num_input_channels=len(encoding_layers), num_vz=3)
+
+# Create train DAG
 tp_vcf = VCFLabelLoader.VcfBamPaths(vcf="/ssd/VariantWorks/end_to_end_workflow_sample_files/tp_vcf_1m_giab.vcf.gz", bam=bam, is_fp=False)
 fp_vcf = VCFLabelLoader.VcfBamPaths(vcf="/ssd/VariantWorks/end_to_end_workflow_sample_files/train_fp.vcf.gz", bam=bam, is_fp=True)
 vcf_loader = VCFLabelLoader([tp_vcf, fp_vcf])
 print(len(vcf_loader))
 label_loader_time = time.time()
 print("Label loading time {}".format(label_loader_time - start_time))
-zyg_encoder = ZygosityLabelEncoder()
-train_dataset = VariantDataLoader(pileup_encoder, vcf_loader, zyg_encoder, batch_size=32, shuffle=False, num_workers=32)
-
-# Setup loss
+train_dataset = VariantDataLoader(pileup_encoder, vcf_loader, zyg_encoder, batch_size=32, shuffle=True, num_workers=32)
 vz_ce_loss = CrossEntropyLossNM(logits_ndim=2)
-
-# Neural Network
-alexnet = AlexNet(num_input_channels=len(encoding_layers), num_vz=3)
-
-# Create train DAG
 vz_labels, encoding = train_dataset()
 vz = alexnet(encoding=encoding)
 vz_loss = vz_ce_loss(logits=vz, labels=vz_labels)
 
-# SimpleLossLoggerCallback will print loss values to console.
-def my_print_fn(x):
-    acc = compute_accuracy(x)
-    logging.info(f'Train VT Loss: {str(x[0].item())}, Accuracy : {str(acc)}')
+# Create eval DAG
+eval_tp_vcf = VCFLabelLoader.VcfBamPaths(vcf="/ssd/VariantWorks/end_to_end_workflow_sample_files/test_tp_vcf_100k_giab.vcf.gz", bam=bam, is_fp=False)
+eval_fp_vcf = VCFLabelLoader.VcfBamPaths(vcf="/ssd/VariantWorks/end_to_end_workflow_sample_files/test_fp_vcf_100k_giab.vcf.gz", bam=bam, is_fp=True)
+eval_vcf_loader = VCFLabelLoader([eval_tp_vcf, eval_fp_vcf])
+print(len(eval_vcf_loader))
+label_loader_time = time.time()
+print("Label loading time {}".format(label_loader_time - start_time))
+eval_dataset = VariantDataLoader(pileup_encoder, eval_vcf_loader, zyg_encoder, batch_size=128, shuffle=False, num_workers=32)
+eval_vz_ce_loss = CrossEntropyLossNM(logits_ndim=2)
+eval_vz_labels, eval_encoding = train_dataset()
+eval_vz = alexnet(encoding=eval_encoding)
+eval_vz_loss = eval_vz_ce_loss(logits=eval_vz, labels=eval_vz_labels)
 
 # Logger callback
 loggercallback = nemo.core.SimpleLossLoggerCallback(
         tensors=[vz_loss, vz, vz_labels],
-        print_func=my_print_fn,
         step_freq=1,
         )
 
@@ -77,9 +100,16 @@ checkpointcallback = nemo.core.CheckpointCallback(
         force_load=False
         )
 
+evaluator_callback = nemo.core.EvaluatorCallback(
+        eval_tensors=[eval_vz_loss, eval_vz, eval_vz_labels],
+        user_iter_callback=eval_iter_callback,
+        user_epochs_done_callback=eval_epochs_done_callback,
+        eval_step=1000,
+        )
+
 # Invoke the "train" action.
 nf.train([vz_loss],
-         callbacks=[loggercallback, checkpointcallback],
+         callbacks=[loggercallback, checkpointcallback, evaluator_callback],
          optimization_params={"num_epochs": 1, "lr": 0.001},
          optimizer="adam")
 
