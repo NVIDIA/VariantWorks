b3e03ac670dde8cf43cd99174ffd8f850a6a9171
Joyjit Daw <jdaw@nvidia.com>
2020-07-24 22:07:23 -0400

    [vcfio] fully featured VCF data frame
    1. convert all columns to data frame friendly format
    2. update vcfreader getitem function to convert to dataclass correctly

--- variantworks/io/vcfio.py
+++ variantworks/io/vcfio.py
@@ -28,7 +28,7 @@ from variantworks.utils import extend_exception
 class VCFReader(BaseReader):
     """Reader for VCF files."""
 
-    def __init__(self, vcf, bams=[], is_fp=False, require_genotype=True, tags=[]):
+    def __init__(self, vcf, bams=[], is_fp=False, require_genotype=True, tag="caller"):
         """Parse and extract variants from a vcf/bam tuple.
 
         Note -VCFReader splits multi-allelic entries into separate variant
@@ -40,7 +40,7 @@ class VCFReader(BaseReader):
                    ordering in VCF.
             is_fp : Is the VCF for false positive variants.
             require_genotype : If all samples need genotype called.
-            tags : List of strings to tag VCF data frame with.
+            tag : Tag VCF data frame with.
 
         Returns:
            Instance of class.
@@ -50,10 +50,22 @@ class VCFReader(BaseReader):
         self._bams = bams
         self._is_fp = is_fp
         self._require_genotype = require_genotype
-        self._tags = tags
+        self._tag = tag
         self._labels = []
         self._dataframe = None
+
+        self._filter_df_keys = set()
+        self._filter_vcf_keys = set()
+
+        self._call_df_keys = set()
+        self._call_vcf_keys = dict()
+        self._call_names = set()
+
+        self._info_df_keys = set()
+        self._info_vcf_keys = dict()
+
         self._parse_vcf()
+
 
     def __getitem__(self, idx):
         """Get Variant instance in location.
@@ -64,20 +76,49 @@ class VCFReader(BaseReader):
             Variant instance
         """
         row = self._dataframe.iloc[idx]
+
+        # Build sample data
         samples = []
         zygosities = []
-        for i in range(row["num_samples"]):
-            samples.append(row["sample_{}_call".format(i)])
-            zygosities.append(row["sample_{}_zyg".format(i)])
+        format_keys = sorted(self._call_vcf_keys.keys())
+        for call in self._call_names:
+            call_data = []
+            for k in format_keys:
+                count = self._call_vcf_keys[k]
+                if count == 1:
+                    call_data.append(row["{}_{}_{}".format(self._tag, call, k)])
+                else:
+                    for i in range(count):
+                        call_data.append(row["{}_{}_{}_{}".format(self._tag, call, k, i)])
+            samples.append(call_data)
+            zygosities.append(row["{}_{}_zyg".format(self._tag, call)])
+
+        # Build filter data
+        var_filter = []
+        for k in self._filter_vcf_keys:
+            if row["{}_FILTER_{}".format(self._tag, k)]:
+                var_filter.append(k)
+
+        # Build info data
+        info = {}
+        for k, count in self._info_vcf_keys.items():
+            if count == 1:
+                info[k] = row["{}_INFO_{}".format(self._tag, k)]
+            else:
+                vals = []
+                for i in range(count):
+                    vals.append(row["{}_INFO_{}_{}".format(self._tag, k, i)])
+                info[k] = vals
+
         variant = Variant(chrom=row["chrom"],
                           pos=row["start_pos"],
                           id=row["id"],
                           ref=row["ref"],
                           allele=row["alt"],
-                          quality=row["quality"],
-                          filter=row["filter"],
-                          info=row["info"],
-                          format=row["format"],
+                          quality=row["{}_quality".format(self._tag)],
+                          filter=(var_filter if var_filter else None),
+                          info=info,
+                          format=format_keys,
                           type=row["variant_type"],
                           samples=samples,
                           zygosity=zygosities,
@@ -164,7 +205,100 @@ class VCFReader(BaseReader):
                 return VariantType.INSERTION
         raise ValueError("Unexpected variant type - {}".format(record))
 
-    def _add_variant_to_dict(self, df_dict, record):
+    @staticmethod
+    def _extend_list(l, expected_length, default_value):
+        if len(l) != expected_length:
+            l += [default_value] * (expected_length - len(l))
+
+    def _finalize_filters(self, df_dict, total_entries):
+        for k in self._filter_df_keys:
+            self._extend_list(df_dict[k], total_entries, False)
+
+    def _add_filter(self, df_dict, vcf_filter, idx):
+        keys = []
+        values = []
+        if vcf_filter is None:
+            pass
+        elif vcf_filter == []:
+            keys.append("{}_FILTER_PASS".format(self._tag))
+            values.append(True)
+
+            self._filter_vcf_keys.add("PASS")
+        else:
+            for k in vcf_filter:
+                keys.append("{}_FILTER_{}".format(self._tag, k))
+                values.append(True)
+
+                self._filter_vcf_keys.add(k)
+
+        for k, v in zip(keys, values):
+            self._extend_list(df_dict[k], idx, False)
+            df_dict[k].append(v)
+
+            self._filter_df_keys.add(k)
+
+    def _finalize_calls(self, df_dict, total_entries):
+        for k in self._call_df_keys:
+            self._extend_list(df_dict[k], total_entries, None)
+
+    def _add_call(self, df_dict, format_str, sample, idx):
+        name = sample.sample
+
+        self._call_names.add(name)
+
+        keys = []
+        values = []
+
+        for col, data in zip(format_str, sample.data):
+            if isinstance(data, list):
+                for i, val in enumerate(data):
+                    keys.append("{}_{}_{}_{}".format(self._tag, name, col, i))
+                    values.append(val)
+
+                self._call_vcf_keys[col] = len(data)
+            else:
+                keys.append("{}_{}_{}".format(self._tag, name, col))
+                values.append(data)
+
+                self._call_vcf_keys[col] = 1
+
+        if self._require_genotype:
+            keys.append("{}_{}_zyg".format(self._tag, name))
+            values.append(self._get_variant_zygosity(sample))
+
+        for k, v in zip(keys, values):
+            self._extend_list(df_dict[k], idx, None)
+            df_dict[k].append(v)
+
+            self._call_df_keys.add(k)
+
+    def _finalize_info(self, df_dict, total_entries):
+        for k in self._info_df_keys:
+            self._extend_list(df_dict[k], total_entries, None)
+
+    def _add_info(self, df_dict, info, idx):
+        keys = []
+        values = []
+        for k, v in info.items():
+            if isinstance(v, list) and len(v) > 1:
+                for i in range(len(v)):
+                    keys.append("{}_INFO_{}_{}".format(self._tag, k, i))
+                    values.append(v[i])
+
+                self._info_vcf_keys[k] = len(v)
+            else:
+                keys.append("{}_INFO_{}".format(self._tag, k))
+                values.append(v)
+
+                self._info_vcf_keys[k] = 1
+
+        for k, v in zip(keys, values):
+            self._extend_list(df_dict[k], idx, None)
+            df_dict[k].append(v)
+
+            self._info_df_keys.add(k)
+
+    def _add_variant_to_dict(self, df_dict, record, idx):
         """Create a variant record from pyVCF record.
 
         Args:
@@ -184,6 +318,7 @@ class VCFReader(BaseReader):
                     raise RuntimeError("Could not parse format field for entry - {}".format(record))
 
             try:
+                # Common columns
                 df_dict["chrom"].append(record.CHROM)
                 df_dict["start_pos"].append(record.POS)
                 df_dict["end_pos"].append(record.POS + 1)
@@ -191,18 +326,13 @@ class VCFReader(BaseReader):
                 df_dict["ref"].append(record.REF)
                 df_dict["alt"].append(var_allele)
                 df_dict["variant_type"].append(var_type)
-                df_dict["quality"].append(record.QUAL)
-                df_dict["filter"].append(record.FILTER)
-                df_dict["info"].append(record.INFO)
-                df_dict["format"].append(var_format)
-                df_dict["num_samples"].append(len(record.samples))
-                for i, sample in enumerate(record.samples):
-                    df_dict["sample_{}_call".format(i)].append(
-                        [field_value for field_value in sample.data]
-                    )
-                    df_dict["sample_{}_zyg".format(i)].append(
-                        self._get_variant_zygosity(sample)
-                    )
+                df_dict["{}_quality".format(self._tag)].append(record.QUAL)
+                # Columns that vary by VCF
+                self._add_info(df_dict, record.INFO, idx)
+                self._add_filter(df_dict, record.FILTER, idx)
+                #df_dict["info"].append(record.INFO)
+                for sample in record.samples:
+                    self._add_call(df_dict, var_format, sample, idx)
             except Exception as e:
                 raise extend_exception(e, "Could not parse variant from entry - {}".format(record)) from None
 
@@ -229,12 +359,22 @@ class VCFReader(BaseReader):
         try:
             vcf_reader = self._get_file_reader(vcf_file_path=self._vcf)
             df_dict = defaultdict(list)
-            for record in vcf_reader:
-                self._add_variant_to_dict(df_dict, record)
+            record_counter = 0
+            for idx, record in enumerate(vcf_reader):
+                self._add_variant_to_dict(df_dict, record, idx)
+                #if record_counter == 400:
+                #    record_counter += 1
+                #    break
+                record_counter += 1
+            self._finalize_filters(df_dict, record_counter)
+            self._finalize_calls(df_dict, record_counter)
+            self._finalize_info(df_dict, record_counter)
 
             self._dataframe = pd.DataFrame(df_dict)
-            for tag in self._tags:
-                self._dataframe[tag] = 1
+            self._dataframe[self._tag] = 1
+
+            #print(self._dataframe)
+            #print(self._info_df_keys)
 
         except Exception as e:
             # raise from None is used to clear the context of the parent exception.
