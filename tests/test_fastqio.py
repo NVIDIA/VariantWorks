b8aeb7d05b95fdd437e6fd26fd006ab76e8a5d94
Ohad Mosafi <omosafi@nvidia.com>
2020-10-16 15:46:18 -0700

    Add a test for FastqWriter validation

--- tests/test_fastqio.py
+++ tests/test_fastqio.py
@@ -14,9 +14,14 @@
 # limitations under the License.
 #
 
+from Bio import SeqIO
+import itertools
 import numpy as np
+import os
 import pytest
+import tempfile
 
+from variantworks.io.fastxio import FastqWriter
 from variantworks.utils.metrics import convert_error_probability_arr_to_phred
 
 
@@ -32,3 +37,31 @@ def test_err_prob_to_phred_score(err_prob_values, err_prob_values_with_exception
     assert np.array_equal(output, [6, 8, 0, 15, 29, 39, 50])
     with pytest.raises(ValueError):
         convert_error_probability_arr_to_phred(err_prob_values_with_exception)
+
+
+def test_fastq_writer():
+    """Test FASTQ writer"""
+    nucleotides_sequences = ["ACTGATG",
+                             "ACAC"]
+    nucleotides_certainties = [[0.87, 0.424, 0.625, 0.99, 0.001, 0.64, 0.787],
+                               [0.24, 0.53, 0.765, 0.362]]
+    record_ids = ["dl_seq_1",
+                  "dl_seq_2"]
+
+    _, file_path = tempfile.mkstemp(prefix='vw_test_fastq_writer_', suffix='.fastq')
+    try:
+        with FastqWriter(file_path, 'w') as fqout:
+            for record_id, seq, q_score in \
+                    itertools.zip_longest(record_ids, nucleotides_sequences, nucleotides_certainties):
+                fqout.write_output(record_id, seq, q_score)
+        # verify created FASTQ file values
+        for idx, record in enumerate(SeqIO.parse(file_path, "fastq")):
+            assert record_ids[idx] == record.id
+            assert nucleotides_sequences[idx] == record.seq
+            assert np.array_equal(
+                # convert certainties to error rates
+                convert_error_probability_arr_to_phred([1 - val for val in nucleotides_certainties[idx]]),
+                record.letter_annotations["phred_quality"]
+            )
+    finally:
+        os.remove(file_path)
