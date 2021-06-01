43d57ed5f62c04a70ff8d5d92dde7d48067e5286
Avantika Lal <avantikalal1990@gmail.com>
2021-06-01 08:12:21 -0700

    Update networks.py

--- variantworks/networks.py
+++ variantworks/networks.py
@@ -214,7 +214,8 @@ class ConsensusCNN(TrainableNM):
             'output_logit': NeuralType(('B', 'W', 'D'), LogitsType()),
         }
 
-    def __init__(self, input_feature_size, kernel_size, channels, gru_size, conv_layers, num_output_logits, relu=False, apply_softmax=False):
+    def __init__(self, input_feature_size, kernel_size, channels, gru_size, conv_layers, 
+                 num_output_logits, relu=False, apply_softmax=False):
         """Construct an Consensus CNN NeMo instance.
 
         Args:
@@ -233,11 +234,13 @@ class ConsensusCNN(TrainableNM):
         super().__init__()
         self.num_output_logits = num_output_logits
         self.cnn_layers = nn.ModuleList()
-        self.cnn_layers.append(nn.Conv1d(input_feature_size, channels, kernel_size=kernel_size, padding=int((kernel_size-1)/2)))
+        self.cnn_layers.append(nn.Conv1d(input_feature_size, channels, kernel_size=kernel_size, 
+                                         padding=int((kernel_size-1)/2)))
         if relu:
             self.cnn_layers.append(nn.ReLU())
         for _ in range(conv_layers-1):
-            self.cnn_layers.append(nn.Conv1d(channels, channels, kernel_size=kernel_size, padding=int((kernel_size-1)/2)))
+            self.cnn_layers.append(nn.Conv1d(channels, channels, kernel_size=kernel_size, 
+                                             padding=int((kernel_size-1)/2)))
             if relu:
                 self.cnn_layers.append(nn.ReLU())
         self.gru = nn.GRU(channels, gru_size, 1, batch_first=True, bidirectional=True)
